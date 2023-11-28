3f2d2a6ca42e323fc4ace313f010d849d8a93b38
Chiara Puleio <chiarapuleio@yahoo.it>
2023-11-28 16:11:59 +0100

    components and html, fetch WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Route } from '@angular/router';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivePostsComponent } from './components/active-posts/active-posts.component';
+import { InactivePostsComponent } from './components/inactive-posts/inactive-posts.component';
+
+const routes: Route[] = [
+  { path: ' ', component: NavbarComponent },
+  { path: 'active-posts', component: ActivePostsComponent },
+  { path: 'inactive-posts', component: InactivePostsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ActivePostsComponent,
+    InactivePostsComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
